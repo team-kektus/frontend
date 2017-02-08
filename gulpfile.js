162fbe08d6454720a7c8100b8aa526339cdce5cf
Markus Peterson <marcus17777@gmail.com>
2017-02-08 22:56:04 +0200

    Added develoment pipeline.

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,13 @@
 const gulp = require('gulp');
 const connect = require('gulp-connect');
+const sass = require('gulp-sass');
+const inject = require('gulp-inject');
 const proxymiddleware = require('http-proxy-middleware');
+const del = require('del');
 
-const config = require('./gulptasks/config.json');
+const config = require('./config.json');
+const assets = require('bower-files')(config.bowerConfig);
 
-console.log(config);
 
 gulp.task('server', function () {
   connect.server({
@@ -22,4 +25,39 @@ gulp.task('server', function () {
   })
 })
 
-gulp.task('default', ['server'])
+gulp.task('bower:js', function () {
+  return gulp.src(assets.ext('js').files)
+    .pipe(gulp.dest(config.lib.js))
+})
+
+gulp.task('bower:css', function () {
+  return gulp.src(assets.ext(['css', 'scss']).files)
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest(config.lib.css))
+})
+
+gulp.task('bower:fonts', function () {
+  return gulp.src(assets.ext(['eot', 'woff', 'woff2', 'ttf', 'svg']).files)
+    .pipe(gulp.dest(config.lib.fonts))
+})
+
+gulp.task('bower', gulp.parallel('bower:js', 'bower:css', 'bower:fonts'))
+
+gulp.task('inject', function () {
+  return gulp.src(config.app.index)
+    .pipe(inject(gulp.src(config.app.injectorder)))
+    .pipe(gulp.dest(config.app.root))
+})
+
+gulp.task('clean', function (done) {
+  var to_delete = [
+    './app/**/*.css',
+    './app/**/*.map',
+    './app/lib'
+  ]
+  return del(to_delete)
+})
+
+gulp.task('build', gulp.series('clean', 'bower', 'inject'))
+
+gulp.task('default', gulp.series('build', 'server'))
