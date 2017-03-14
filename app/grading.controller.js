cc0cef6b8273e32f4d801817a9320e9dad77dd7c
Kristjan Puusepp <kristjanps96@gmail.com>
2017-03-14 10:50:53 +0200

    Start with grading table

---
+++ app/grading.controller.js
@@ -0,0 +1,19 @@
+/*
+class gradingController {
+  constructor() {
+    console.log('Initializing gradingController');
+  }
+
+  loo_tabel() {
+    this.persons = [
+      {first_name: 'asd', last_name: 'sfd', id: ''},
+      {first_name: 'asd', last_name: 'sfd', id: ''},
+      {first_name: 'asd', last_name: 'sfd', id: ''}
+    ];
+  }
+
+}
+
+angular.module('frontend.grading')
+  .controller('gradingController', [gradingController])
+*/
---
+++ app/grading.js
@@ -0,0 +1,19 @@
+
+(function(){
+    'use strict';
+
+    angular.module('asd', [])
+
+    .controller('AsdCtrl', [ function () {
+
+        this.persons = [
+
+    {first_name: 'asd', last_name: 'sfd', id: ''},
+    {first_name: 'asd', last_name: 'sfd', id: ''},
+    {first_name: 'asd', last_name: 'sfd', id: ''}
+
+];
+
+    }]);
+
+})();
