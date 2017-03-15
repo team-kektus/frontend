9209af96ac002962282d7e7d5bca2e91390fd1d7
Kristjan Puusepp <kristjanps96@gmail.com>
2017-03-15 12:14:17 +0200

    angular table

--- app/grading.js
+++ app/grading.js
@@ -8,9 +8,9 @@
 
         this.persons = [
 
-    {first_name: 'asd', last_name: 'sfd', id: ''},
-    {first_name: 'asd', last_name: 'sfd', id: ''},
-    {first_name: 'asd', last_name: 'sfd', id: ''}
+    {number: '1.1', description: 'Andmete salvestamine andmebaasi',first_name: 'asd', last_name: 'sfd', datetime: '5', esitamise_etapp:'1', id: ''},
+    {number: '1.2', description: 'Andmebaasi andmete esitamine', first_name: 'ttd', last_name: 'sfd', datetime: '5',esitamise_etapp:'1', id: ''},
+    {number: '1.3', description: 'Agregeeritud andmete esitamine',  first_name: 'ptd', last_name: 'sfd', datetime: '6', esitamise_etapp:'1', id: ''}
 
 ];
 
