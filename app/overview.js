2ffc04912ea093e7df6af6ad119dcb712927c9e0
Kristjan Puusepp <kristjanps96@gmail.com>
2017-03-15 13:34:43 +0200

    angular tables

---
+++ app/overview.js
@@ -0,0 +1,17 @@
+(function(){
+    'use strict';
+
+    angular.module('overview', [])
+
+    .controller('OverviewCtrl', [ function () {
+
+        this.teams = [
+
+    {teamname: 'tiiminimi1', teamname2: 'tiiminimi2'},
+    {teamname: 'tiiminimi3', teamname2: 'tiiminimi4'}
+
+];
+
+    }]);
+
+})();
