6b582309313bf49eec49446223e6aae72d8afa5c
Markus Peterson <marcus17777@gmail.com>
2017-03-19 16:22:44 +0200

    Made login transition smoother by removing window.location.reload().

--- app/app.controller.js
+++ app/app.controller.js
@@ -1,19 +1,24 @@
 class MainController {
-  constructor($state, Api) {
+  constructor($rootScope, $location, Api) {
+    this.$rootScope = $rootScope
     this.Api = Api
-    this.$state = $state
+    this.$location = $location
     this.current_year = new Date().getFullYear()
   }
 
   logout() {
     console.log('logout');
     this.Api.logout().then((result) => console.log(result))
+    this.$rootScope.currentUser = null
+    this.$location.path('/login/')
+    // window.location.reload()
   }
 }
 
 angular.module('kektus')
   .controller('MainController', [
-    '$state',
+    '$rootScope',
+    '$location',
     'Api',
     MainController
   ])
