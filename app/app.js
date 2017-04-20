d16813585caad9176469320359c4d76522955c55
Markus Peterson <marcus17777@gmail.com>
2017-04-20 17:23:09 +0300

    Grading aspect interactive help added.

--- app/app.js
+++ app/app.js
@@ -28,4 +28,14 @@ angular.module('kektus')
     $rootScope.Layout = {
       'sidebar_is_closed': false
     }
+
+    // Uses Cors-Anywhere to make cors requests to servers where CORS Access-Control-Allow-Origin is not present.
+    $.ajaxPrefilter( function (options) {
+      if (options.crossDomain && jQuery.support.cors) {
+        var http = (window.location.protocol === 'http:' ? 'http:' : 'https:');
+        options.url = http + '//cors-anywhere.herokuapp.com/' + options.url;
+        //options.url = "http://cors.corsproxy.io/url=" + options.url;
+      }
+    })
+
   })
