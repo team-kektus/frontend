50b36db5d35cfa34ec0f77f7d09e76cdb7d1e926
Markus Peterson <marcus17777@gmail.com>
2017-04-21 12:18:09 +0300

    Added directives to hide content according to user permissions.

--- app/components/protected-content.directive.js
+++ app/components/protected-content.directive.js
@@ -1,11 +1,20 @@
 angular.module('kektus.components')
-  .directive("protected-", [function() {
+  .directive("protectedProfessor", ['$rootScope', function($rootScope) {
     return {
-      scope: {
-        // fileread: "="
-      },
       link: function(scope, element, attributes) {
-        
+        if (!$rootScope.currentUser.is_professor) {
+          element.hide()
+        }
       }
     }
-  }]);
+  }])
+
+  .directive("protectedAdmin", ['$rootScope', function($rootScope) {
+    return {
+      link: function(scope, element, attributes) {
+        if (!$rootScope.currentUser.is_admin) {
+          element.hide()
+        }
+      }
+    }
+  }])
